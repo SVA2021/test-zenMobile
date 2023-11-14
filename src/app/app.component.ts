afa6da4c977b7502503e8a30a209fe677798ded6
Vitaliy S <sofronov.vit@gmail.com>
2023-11-14 21:19:57 +0400

    add project steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,18 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
-export class AppComponent { 
+export class AppComponent {
 }
+
+// TODO: add initial configs
+// TODO: add initial style variables
+// TODO: add header component
+// TODO: add nav menu component with icons
+// TODO: add home page module with pages
+// TODO: add edit profile page and find where to put link to it
+// TODO: add auth page
+// TODO: add fake auth service
+// TODO: add error handling and fake error generation
+// TODO: add local storage service for auth data
+// TODO: check project
+// TODO: upload to github
