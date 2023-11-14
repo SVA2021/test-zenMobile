8e4e0af900963f95259b958c89796d69458e3a10
Vitaliy S <sofronov.vit@gmail.com>
2023-11-14 20:44:44 +0400

    initial project setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from 'core/core.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CoreModule,
+        BrowserAnimationsModule,
+        TuiRootModule,
+        TuiDialogModule,
+        TuiAlertModule
+    ],
+    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
